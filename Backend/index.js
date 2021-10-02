7fd713a2b99206552e1a51d17a22da8e8592b9eb
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-10-02 16:50:45 +0530

    folder structure + schema

--- Backend/index.js
+++ Backend/index.js
@@ -5,9 +5,9 @@ const express = require("express");
 const app = express();
 const port = 5000;
 
-app.get("/", (req, res) => {
-  res.send("Hello Vaibhav!");
-});
+//Available routes
+app.use("/api/auth", require("./routes/auth"));
+app.use("/api/notes", require("./routes/notes"));
 
 app.listen(port, () => {
   console.log(`App listening at http://localhost:${port}`);
