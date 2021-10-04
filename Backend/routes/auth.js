d7bf612aa25977f1001b1e6c008dc46033dc71b6
Vaibhav Magar <vaibhavmagar1901@gmail.com>
2021-10-04 07:26:58 +0530

    backend completed successfully!

--- Backend/routes/auth.js
+++ Backend/routes/auth.js
@@ -4,9 +4,12 @@ const User = require("../models/User");
 const { body, validationResult } = require("express-validator");
 const bcrypt = require("bcryptjs");
 const jwt = require("jsonwebtoken");
+const fetchUser = require("../middlewares/fetchUser");
 
-// Create a User using : POST "/api/auth/createuser". Doesn't require auth - No Login Required
+//JWT_SECRET key
 const JWT_SECRET = "vaibhavmagar123";
+
+//ROUTE 1 : Create a User using : POST " /api/auth/createuser". Doesn't require auth - No Login Required
 
 router.post(
   "/createuser",
@@ -54,9 +57,75 @@ router.post(
       //   res.json(user);
     } catch (error) {
       console.error(error.message);
-      res.status(500).send("Some Error Occured");
+      res.status(500).send("Internal Server Error");
     }
   }
 );
 
+//ROUTE 2 :--> Authenticate a user:--> POST "/api/auth/login" :--> no login required
+router.post(
+  "/login",
+  [
+    body("email", "Enter a valid email").isEmail(),
+    body("password", "Password cannot be blank").exists(),
+  ],
+  async (req, res) => {
+    //checks entered values are true or not - if not then return erros
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { email, password } = req.body;
+
+    try {
+      let user = await User.findOne({ email });
+      if (!user) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+
+      const comparePass = await bcrypt.compare(password, user.password);
+      if (!comparePass) {
+        return res
+          .status(400)
+          .json({ error: "Please login with correct credentials" });
+      }
+
+      const data = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      const authToken = jwt.sign(data, JWT_SECRET);
+      res.json({ authToken });
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//ROUTE 3 :--> get user details:--> POST "/api/auth/getuser" :-->  login required
+router.post("/getuser", fetchUser, async (req, res) => {
+  try {
+    userId = req.user.id;
+    const user = await User.findById(userId).select("-password");
+    res.send(user);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
 module.exports = router;
+
+/*
+
+{
+  "email": "harryyyy123@gmail.com",
+  "password": "haryyyy1232"
+}
+
+*/
--- Backend/routes/notes.js
+++ Backend/routes/notes.js
@@ -1,12 +1,128 @@
 const express = require("express");
 const router = express.Router();
+const fetchUser = require("../middlewares/fetchUser");
+const Note = require("../models/Note");
+const { body, validationResult } = require("express-validator");
 
-router.get("/", (req, res) => {
-  obj = {
-    name: "Vaibhav",
-    age: 19,
-  };
-  res.json(obj);
+//ROUTE 1 : Get all notes  : GET " /api/notes/fetchAllNotes" Login Required
+
+router.get("/fetchallnotes", fetchUser, async (req, res) => {
+  try {
+    const notes = await Note.find({ user: req.user.id });
+    res.json(notes);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//ROUTE 2 : Add a new note  : POST " /api/notes/addnote" Login Required
+
+router.post(
+  "/addnote",
+  fetchUser,
+  [
+    body("title", "Enter a valid title").isLength({ min: 5 }),
+    body(
+      "description",
+      "Description must be of atleasat 5 characters"
+    ).isLength({
+      min: 5,
+    }),
+  ],
+  async (req, res) => {
+    try {
+      const { title, description, tag } = req.body;
+
+      //checks entered values are true or not - if not then return erros
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
+
+      const note = new Note({
+        title,
+        description,
+        tag,
+        user: req.user.id,
+      });
+
+      const savedNote = await note.save();
+
+      res.json(savedNote);
+    } catch (error) {
+      console.error(error.message);
+      res.status(500).send("Internal Server Error");
+    }
+  }
+);
+
+//ROUTE 3 : Update an existing note  : POST " /api/notes/updatenote" Login Required
+
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  try {
+    //create a newNote Object
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+
+    //Find the noSte which is to be updated and update it
+    let note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+
+    if (note.user.toString() != req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndUpdate(
+      req.params.id,
+      { $set: newNote },
+      { new: true }
+    );
+
+    res.json({ note });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
+
+//ROUTE 4 : Delete an existing note  : DELETE " /api/notes/updatenote" Login Required
+
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  try {
+    //Find the noSte which is to be updated and update it
+    let note = await Note.findById(req.params.id);
+
+    if (!note) {
+      return res.status(404).send("Not found");
+    }
+
+    if (note.user.toString() != req.user.id) {
+      return res.status(401).send("Not Allowed");
+    }
+
+    note = await Note.findByIdAndDelete(req.params.id);
+
+    res.json({ Success: "Note has been deleted" });
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 module.exports = router;
